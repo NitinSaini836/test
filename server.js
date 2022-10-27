c0556c2fc72a262482cc94a490afa51ddabb0a4a
Nitin Saini <Nitin.Saini@softobiz.com>
2022-10-27 17:27:46 +0530

    with ejs

--- server.js
+++ server.js
@@ -1,33 +1,26 @@
 const express = require('express');
 const db = require('./db')
+
+let ejs = require('ejs');
 const PORT = 9000;
 const userModel = require('./message')
 
-
 let app = express()
-
-
-db.init();
-app.get('/',(req,res)=>{
-res.send("hello")
-})
-// app.route('/index.js')
-// .get((req, res) => {
-// {
-//     userModel.find().pretty()
-// 		.then(function(data)
-//     {
-//       console.log(data)
-//       res.send(data)
-    
-//     }).catch(function(err)
-//     {
-			
-//      console.log(err)
-//     })
-//   }
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+db.connexion();
+// app.get('/',(req,res)=>{
+// res.send("hello")
 // })
-  
+app.get("/", (req, res) => {
+ userModel.find({}, (err, result)=>{
+    if(err) throw err;
+    res.render('view.ejs', {result : result})
+    // res.status(200).send(result)
+ });
+})
+
 
 app.listen(PORT,(req,res)=>{
     console.log(`app listening on port:${PORT}`);
